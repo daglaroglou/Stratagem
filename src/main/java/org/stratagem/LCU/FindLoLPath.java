5d947e63c917072fcbe607b9490868a4d4d15dc7
Christos Daglaroglou <christos.daglaroglou@gmail.com>
2025-07-25 13:07:16 +0300

    Refactor FindLoLPath class and update package structure

--- src/main/java/org/stratagem/LCU/FindLoLPath.java
+++ src/main/java/org/stratagem/LCU/FindLoLPath.java
@@ -1,11 +1,11 @@
-package org.stratagem.utils;
+package org.stratagem.LCU;
 
 import java.util.Optional;
 
 public class FindLoLPath {
 
-    public static String findLeagueClientPath() {
-        return ProcessHandle.allProcesses()
+    public static String findLeagueInstallationPath() {
+        String clientPath = ProcessHandle.allProcesses()
                 .map(ProcessHandle::info)
                 .map(ProcessHandle.Info::command)
                 .filter(Optional::isPresent)
@@ -13,5 +13,10 @@ public class FindLoLPath {
                 .filter(cmd -> cmd.toLowerCase().endsWith("leagueclient.exe"))
                 .findFirst()
                 .orElse(null);
+
+        if (clientPath == null) return null;
+        int idx = clientPath.toLowerCase().lastIndexOf("leagueclient.exe");
+        if (idx == -1) return null;
+        return clientPath.substring(0, idx);
     }
 }
--- src/main/java/org/stratagem/Main.java
+++ src/main/java/org/stratagem/Main.java
@@ -1,14 +1,9 @@
 package org.stratagem;
 
-import org.stratagem.utils.FindLoLPath;
+import org.stratagem.LCU.*;
 
 public class Main {
     public static void main(String[] args) {
-        String path = FindLoLPath.findLeagueClientPath();
-        if (path != null) {
-            System.out.println("League of Legends client found at: " + path);
-        } else {
-            System.out.println("League of Legends client not found.");
-        }
+        System.out.println("Stratagem");
     }
 }
